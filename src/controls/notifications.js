9d5bf86fac7b2f32f2522748da7bfbf3df14bfc5
Pcunvik <peter.cunvik@hotmail.com>
2021-06-18 14:36:17 +0200

    feat: add realtime support for sensor data

--- src/controls/notifications.js
+++ src/controls/notifications.js
@@ -1,43 +1,61 @@
 import { io } from 'socket.io-client';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
+import { Circle, Stroke, Style } from 'ol/style';
 import GeoJSONFormat from 'ol/format/GeoJSON';
 import { Component } from '../ui';
 
 const Notifications = function Notifications(options = {}) {
   let viewer;
-
+  const geoJsonStyle = new Style({
+    image: new Circle({
+      fill: null,
+      stroke: new Stroke({ color: 'blue', width: 3 }),
+      radius: 13
+    }),
+    fill: null,
+    stroke: new Stroke({ color: 'blue', width: 3 })
+  });
   return Component({
     name: 'notifications',
     onAdd(evt) {
       viewer = evt.target;
       let vectorLayer;
       const map = viewer.getMap();
-      const socket = io('http://localhost:3001');
-      socket.on('connect', () => {
-        console.log('connected');
-      });
-      socket.on('DRAW-GEOMETRY', (value) => {
-        console.log(value);
-        console.log('draw geometry event');
-        const format = new GeoJSONFormat({ featureProjection: 'EPSG:3857' });
-        const vectorSource = new VectorSource({
-          features: format.readFeatures(value[0].location)
+      const socket = io(options.webSocketEndpoint);
+
+      // Listen to a redraw event from server and trigger a refresh of
+      // relevant WFS-layer
+      socket.on('redraw-layer', (layerToReload) => {
+        const layers = map.getLayers();
+        layers.forEach(layer => {
+          const layerProperties = layer.getProperties();
+          if (layerProperties.sourceName === layerToReload.sourceName && layerProperties.name === layerToReload.name) {
+            layer.getSource().refresh();
+          }
         });
-        if (!vectorLayer) {
+      });
+
+      // Listen to draw-geometry event from server and add geometry to the map
+      // Work in progress
+      socket.on('draw-geometry', (data) => {
+        const format = new GeoJSONFormat({ featureProjection: viewer.getProjectionCode() });
+        const vectorSource = new VectorSource({
+          features: format.readFeatures(data.geoJson)
+        });
+        if (vectorLayer) {
+          vectorLayer.setSource(vectorSource);
+        } else {
           vectorLayer = new VectorLayer({
             source: vectorSource,
-            name: 'sooth',
             group: 'root',
-            title: 'truth',
+            title: data.layerTitle,
+            style: geoJsonStyle,
             queryable: true
           });
 
           map.addLayer(vectorLayer);
-        } else {
-          vectorLayer.setSource(vectorSource);
         }
-        map.getView().fit(vectorSource.getExtent());
       });
 
       this.render();
